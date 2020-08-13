52d1800075230d7d5c83f2006c2fc2daae4a7620
Yves Ruosch <yves.ruosch@raiffeisen.ch>
2020-08-13 19:33:40 +0200

    created currying function

--- src/me/ruosch/currying/addChain.js
+++ src/me/ruosch/currying/addChain.js
@@ -1,4 +1,3 @@
-
 /*
 *
 *   A Chain adding function
@@ -6,18 +5,26 @@
 *
 * */
 
-function add(n){
+function add(n) {
 
-    let nF = function(x) {
+    let nF = function (x) {
         return add(x + n);
     };
 
-    nF.valueOf = function() {
+    nF.valueOf = function () {
         return n;
     };
 
     return nF;
-
 }
 
 console.log("Add all together: " + add(5)(10)(15))
+
+
+let addArrow = function (n) {
+    const f = x => add(n + x)
+    f.valueOf = () => n
+    return f;
+}
+console.log("Add all together: " + addArrow(5)(10)(15))
+
