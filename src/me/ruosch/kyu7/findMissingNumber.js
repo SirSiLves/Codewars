9681dd9ad4cf7ee897734d016d2da6d322a2a500
Yves <yves@ruosch.me>
2020-08-15 15:49:30 +0200

    next kata

--- src/me/ruosch/kyu7/findMissingNumber.js
+++ src/me/ruosch/kyu7/findMissingNumber.js
@@ -1,5 +1,6 @@
 /*
 *
+*
 *   Lost number in number sequence
 *   https://www.codewars.com/kata/595aa94353e43a8746000120
 *
@@ -7,7 +8,6 @@
 
 
 function findDeletedNumber(arr, mixArr) {
-
     mixArr = mixArr.sort(function (n1, n2) {
         return n1 - n2;
     });
@@ -27,9 +27,11 @@ function findDeletedNumber(arr, mixArr) {
 
 
 function findDeletedNumber2(a, b) {
-    return [...a, ...b].reduce((c,d) => c ^ d, 0);
+    // return arr.reduce((a, v) => a ^ v, 0) ^ mixArr.reduce((a, v) => a ^ v, 0);
+    return [...a, ...b].reduce(function (n1, n2) {
+        return n1 ^ n2;
+    });
 }
 
 
-// findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 9, 7, 4, 6, 2, 3, 8]);
-findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 2, 20, 21, 22, 23, 24, 26, 27, 28, 29, 3, 30, 31, 32, 33, 4, 5, 6, 7, 8, 9]);
+findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], [1, 9, 14, 7, 13, 4, 6, 2, 3, 8, 10, 11, 12]);
