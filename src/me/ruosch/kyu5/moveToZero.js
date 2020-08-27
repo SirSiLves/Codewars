c9fd32b33bda0d6f48a4936d1bdb2b6fe4a9afcc
Yves Ruosch <yves.ruosch@raiffeisen.ch>
2020-08-27 08:43:50 +0200

    created move to zero

---
+++ src/me/ruosch/kyu5/moveToZero.js
@@ -0,0 +1,29 @@
+var moveZeros = function (arr) {
+    let newArr = [];
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) count++;
+        else newArr.push(arr[i]);
+    }
+
+    for (let j = 0; j < count; j++) newArr.push(0);
+
+    return newArr;
+}
+
+
+console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+
+
+var moveZeros2 = function (arr) {
+    let filtedList = arr.filter(function (num) {
+        return num !== 0;
+    });
+    let zeroList = arr.filter(function (num) {
+        return num === 0;
+    });
+    return filtedList.concat(zeroList);
+}
+
+console.log(moveZeros2([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
